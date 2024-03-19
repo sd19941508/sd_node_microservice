5d9b6361c162ad63e3e78dd854624912f695ab7a
sd19941508 <sd19941508@gmail.com>
2024-03-19 12:33:23 +0530

    test commit

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -3,7 +3,6 @@ describe("CustomerService", () => {
   // This will explain which function we are testing!
   describe("SignIn", () => {
     // Which Scenario we are testing
-    // Which Scenario we are testing
     test("validate user inputs", () => {});
 
     test("Validate response", async () => {});
